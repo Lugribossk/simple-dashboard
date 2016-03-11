6438030e108f38d523f3226433e1789a496b1d82
Bo Gotthardt <mail@gotthardt.bo>
2016-03-11 14:24:22 +0100

    Removed favicon

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import ReactDOM from "react-dom";
 import StatusDashboard from "./app/StatusDashboard";
 import "./style.less";
 
-import "./favicon.png";
 import "./touch-icon.png";
 
 ReactDOM.render(
